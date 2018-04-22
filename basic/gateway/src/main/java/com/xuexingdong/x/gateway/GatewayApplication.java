e6bd584833ac1617bbf750e5469ede61fd3f90a8
xuexingdong <306947352@qq.com>
2018-04-22 23:05:05 +0800

    gateway

--- basic/gateway/src/main/java/com/xuexingdong/x/gateway/GatewayApplication.java
+++ basic/gateway/src/main/java/com/xuexingdong/x/gateway/GatewayApplication.java
@@ -2,12 +2,11 @@ package com.xuexingdong.x.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringCloudApplication
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
