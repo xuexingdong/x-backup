aa7ca9210b67978d655974b5fe3f1663dee46c19
xuexingdong <306947352@qq.com>
2018-04-26 08:20:11 +0800

    gateway fix

---
+++ basic/gateway/src/main/java/com/xuexingdong/x/gateway/filter/LoggerFilter.java
@@ -0,0 +1,23 @@
+package com.xuexingdong.x.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggerFilter implements GatewayFilter {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("uri: ", exchange.getRequest().getURI());
+        logger.info("ip: ", exchange.getRequest().getRemoteAddress());
+        return chain.filter(exchange);
+    }
+}
