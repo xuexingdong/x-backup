40582d08f93a6c48a30e073227133bd405ec7c92
xuexingdong <306947352@qq.com>
2018-10-11 09:12:14 +0800

    modify init sql

--- services/backend/src/main/resources/db/migration/V0001__schema_init.sql
+++ services/backend/src/main/resources/db/migration/V0001__schema_init.sql
@@ -22,12 +22,13 @@ CREATE TABLE `rel_user_role` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id`         char(32) COLLATE utf8mb4_unicode_ci     NOT NULL,
-  `username`   varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password`   varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `salt`       varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `points`     int(10) unsigned                        NOT NULL DEFAULT '0',
-  `created_at` datetime                                NOT NULL,
+  `id`          char(32) COLLATE utf8mb4_unicode_ci     NOT NULL,
+  `username`    varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remark_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password`    varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `salt`        varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `points`      int(10) unsigned                        NOT NULL DEFAULT '0',
+  `created_at`  datetime                                NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_username` (`username`) USING BTREE
 )
