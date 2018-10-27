6901ad8dc7473b3577b0f73b055b341c5f4a0dc8
xuexingdong <306947352@qq.com>
2018-10-27 17:04:10 +0800

    get users

--- services/admin/src/main/java/com/xuexingdong/x/admin/controller/UserController.java
+++ services/admin/src/main/java/com/xuexingdong/x/admin/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Validated
@@ -24,8 +26,18 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<UserVO> get(@RequestParam(defaultValue = "0") int page, @RequestParam(value = "per_page", defaultValue = "10") int perPage) {
+    public ResponseEntity<List<UserVO>> get(@RequestParam(defaultValue = "0") int page, @RequestParam(value = "per_page", defaultValue = "10") int perPage) {
         Page<User> userPage = userService.getPageable(PageRequest.of(page, perPage, Sort.by(Sort.Order.desc("created_at"))));
-        return null;
+        List<UserVO> userVOs = userPage.getContent().stream().map(user -> {
+            UserVO userVO = new UserVO();
+            userVO.setId(user.getId());
+            userVO.setPoints(user.getPoints());
+            userVO.setRemarkName(user.getRemarkName());
+            userVO.setCreatedAt(user.getCreatedAt());
+            return userVO;
+        }).collect(Collectors.toList());
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(userPage.getTotalElements()))
+                .body(userVOs);
     }
 }
